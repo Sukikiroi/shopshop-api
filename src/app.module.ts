fd1d2187c5ab7b0b48431975845bb51dc3d947b2
Sukikiroi <kaddourabdellaziz@gmail.com>
2022-01-05 08:49:41 +0100

    create models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { ProductModule } from './product/product.module';
+import { CommandsModule } from './commands/commands.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, ProductModule, CommandsModule],
   controllers: [AppController],
   providers: [AppService],
 })
